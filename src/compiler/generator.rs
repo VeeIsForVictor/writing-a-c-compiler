07e3ae0689553aef5b12d59a8e79f03b6d22206e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-23 13:53:27 +0800

    feat: define assembly abstract syntax tree

--- src/compiler/generator.rs
+++ src/compiler/generator.rs
@@ -1,1 +1,21 @@
+#[derive(Debug)]
+pub enum AOperandNode {
+    Imm(usize),
+    Register,
+}
 
+#[derive(Debug)]
+pub enum AInstructionNode {
+    Mov(AOperandNode, AOperandNode),
+    Ret,
+}
+
+#[derive(Debug)]
+pub enum AFunctionDefinitionNode {
+    Function(String, Vec<AInstructionNode>),
+}
+
+#[derive(Debug)]
+pub enum AProgramNode {
+    Program(AFunctionDefinitionNode),
+}
