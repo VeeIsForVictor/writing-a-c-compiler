886bffeec11f02825083d67fa76c4327294854bd
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-25 18:12:43 +0800

    feat: implement function prologue and epilogue emitters

--- src/compiler/emitter.rs
+++ src/compiler/emitter.rs
@@ -9,10 +9,21 @@ pub fn emit_program(a_program: AProgramNode, output: &mut String) {
     output.push_str("   .section .note.GNU-stack,\"\",@progbits\n");
 }
 
+fn emit_prologue(output: &mut String) {
+    output.push_str(&format!("    pushq %rbp\n"));
+    output.push_str(&format!("    movq %rsp, %rbp\n"));
+}
+
+fn emit_epilogue(output: &mut String) {
+    output.push_str(&format!("   movq %rbp, %rsp\n"));
+    output.push_str(&format!("   popq %rbp\n"));
+}
+
 fn emit_function(a_function: AFunctionDefinitionNode, output: &mut String) {
     let AFunctionDefinitionNode::Function(name, instructions) = a_function;
     output.push_str(&format!("   .globl {name}\n"));
     output.push_str(&format!("{name}:\n"));
+    emit_prologue(output);
     for a_instruction in instructions {
         emit_instructions(a_instruction, output);
     }
@@ -26,6 +37,7 @@ fn emit_instructions(a_instruction: AInstructionNode, output: &mut String) {
             output.push_str(&format!("   movl    {src}, {dst}"));
         }
         AInstructionNode::Ret => {
+            emit_epilogue(output);
             output.push_str("   ret");
         }
         AInstructionNode::Unary(operator, operand) => {
