73cfce2c7499758e06edd973de8448024c988cca
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-27 17:44:44 +0800

    feat: implement Display for ARegisterNode

--- src/compiler/asm_tree.rs
+++ src/compiler/asm_tree.rs
@@ -1,7 +1,18 @@
+use std::fmt::{self, Display};
+
 #[derive(Debug, Clone)]
 pub enum ARegisterNode {
     AX,
     R10,
+}
+
+impl Display for ARegisterNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ARegisterNode::AX => write!(f, "%eax"),
+            ARegisterNode::R10 => write!(f, "%r10d"),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
