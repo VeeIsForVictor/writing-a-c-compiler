e7cfac8168d98f667a0f5ba09f68ff8d12e9a90e
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-12-27 18:32:21 +0800

    feat: add TryFrom implementation for SymbolToken

--- src/compiler/tokens.rs
+++ src/compiler/tokens.rs
@@ -24,6 +24,27 @@ pub enum SymbolToken {
     Tilde,
 }
 
+impl TryFrom<char> for SymbolToken {
+    type Error = &'static str;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        use SymbolToken::*;
+        match value {
+            '(' => Ok(OpenParen),
+            ')' => Ok(CloseParen),
+            '{' => Ok(OpenBrace),
+            '}' => Ok(CloseBrace),
+            ';' => Ok(Semicolon),
+            '\"' => Ok(Quote),
+            '/' => Ok(CommentSymbol),
+            '\n' | ' ' | '\t' => Ok(Whitespace),
+            '-' => Ok(Minus),
+            '~' => Ok(Tilde),
+            _ => Err("coercion from char to SymbolToken failed"),
+        }
+    }
+}
+
 #[derive(Debug)]
 pub enum KeywordToken {
     Int,
